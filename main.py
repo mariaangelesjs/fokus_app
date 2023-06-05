18e78dc1cff507baac14ab0600047bef8a96ae95
mariaangelesjs <mariaangelesjs@gmail.com>
2023-06-05 08:58:14 +0200

    create this

---
+++ main.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python
+
+# Import packages
+from urllib.request import urlopen
+from flask import Flask, request, session, render_template, url_for, redirect
+import uuid
+import datetime
+from datetime import timedelta
+from sources.blobs import get_data
+from flask_login import LoginManager
+import logging
+from azure.storage.blob import BlobServiceClient
+import os
+import pandas as pd
+from sources.blobs import get_data
+import gc
+from io import BytesIO
+from azure.storage.blob import BlobServiceClient
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+import openai
+import streamlit as st
+from streamlit_chat import message
+
+app = Flask(__name__)
+
+uid_secret_key = str(uuid.uuid4())
+
+app.secret_key = uid_secret_key
+
+# Start logger
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Suppress info from blob storage logger
+
+blob_logger = logging.getLogger(
+    'azure.core.pipeline.policies.http_logging_policy')
+blob_logger.setLevel(logging.WARNING)
+
+# Get environment variables
+KVUri = f'https://bas-analyse.vault.azure.net'
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=KVUri, credential=credential)
+STORAGEACCOUNTURL = client.get_secret('storageAccountURL').value
+STORAGEACCOUNTKEY = client.get_secret('storageFokusString').value
+STORAGESTRING = client.get_secret('storageAnalyseString').value
+CONTAINERNAME = '***CONTAINER***'
+
+openai.api_type = "azure"
+openai.api_key = client.get_secret('fokusGPT').value
+openai.api_base = client.get_secret('gptendpoint').value
+openai.api_version = client.get_secret('gptversion').value
+
+# Get input data
+
+
+@app.route('/', methods=['GET', 'POST'])
+def welcome():
+    if request.method == 'POST':
+        # getting input with name = fname in HTML form
+        session['name'] = request.form.get('name')
+        session['email'] = request.form.get('e-post')
+        # getting input with name = lname in HTML form
+        session['phone'] = request.form.get('phone')
+        session['industry'] = request.form.get('industry')
+        session['work-position'] = request.form.get('work-position')
+        fokus = pd.read_parquet(get_data(
+            STORAGEACCOUNTURL, STORAGEACCOUNTKEY,
+            CONTAINERNAME, 'output/fokus-snapshot/FokusKategorierMemento.parquet'))
+        fokus_segments = ['environmentFriendly', 'levelOfImpulsivity',
+                          'levelOfCulture', 'giveToCharity',
+                          'giveToChildrenCharity', 'giveToCatastrophe',
+                          'priceConscious', 'lowPriceSeeker',
+                          'offerSeeker', 'levelOfFeelingsDriven',
+                          'movingProbability', 'buyCar6m',
+                          'levelOfMovility', 'levelOfOpenness',
+                          'levelOfSocialConformity', 'dogProbability',
+                          'catProbability', 'internationalTravel',
+                          'introvertProbability',
+                          'disposableIncomeIndividual', 'disposableIncomeFamily']
+        global person
+        try:
+            person = fokus[fokus['KR_Phone_Mobile'] ==
+                           int(session['phone'])][fokus_segments]
+        except:
+            person = fokus.sample(1, random_state=42)[fokus_segments]
+        return redirect(url_for('prompt'))
+    return render_template('form.html')
+
+
+@app.route('/prompt_generation', methods=['GET', 'POST'])
+def prompt():
+    if request.method == 'POST':
+        session['variable'] = request.form.get('variable')
+        session['words'] = request.form.get('words')
+        session['product'] = request.form.get('product')
+        global prompt_done
+        value = person[session['variable']].replace(
+            {'Lav': 'Low', 'Middels': 'Mild', 'Høy': 'High'}).values[0]
+        prompt_done = str(
+            'Write a ' +
+            session['words'] +
+            ' article selling ' +
+            session['product'] +
+            ' for a person with ' +
+            str(value) + ' in ' +
+            session['variable'] + ' that works as ' +
+            session['work-position'] + ' in ' +
+            session['industry']).replace('_', ' ')
+        return redirect(url_for('fokus_gpt'))
+    return render_template('select_columns.html', columns=person.columns.values, person=person)
+
+
+@app.route('/unique_ad', methods=['GET', 'POST'])
+def fokus_gpt():
+    if 'variable' not in session:
+        return redirect(url_for('prompt'))
+
+    output = openai.ChatCompletion.create(
+        engine='gpt-test',
+        messages=[{"role": "system", "content": "I am Bas AI assistant"},
+                  {"role": "user", "content": prompt_done}],
+        max_tokens=800,
+        stop=['<|im_end|>'])
+
+    answer = output["choices"][0]["message"]["content"].replace(
+        '\n', '').replace(' .', '.').strip()
+
+    # Return answer as JSON
+    return render_template('fokus_gpt.html', answer=answer, prompt=prompt_done)
+
+    # if request.method == 'POST':
+if __name__ == '__main__':
+    app.config['PROPAGATE_EXCEPTIONS'] = True
+    app.run(debug=True)
---
+++ sources/blobs.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python
+
+# Import packages
+from io import BytesIO
+import pickle
+from azure.storage.blob import BlobServiceClient
+import logging
+import datetime
+import bz2file as bz2
+import pickle
+
+
+logger = logging.getLogger(__name__)
+############
+# Functions#
+############
+
+# To download blob from Bas Analyse
+
+
+def get_data(STORAGEACCOUNTURL, STORAGEACCOUNTKEY, CONTAINERNAME, BLOBNAME):
+    blob_service_client_instance = BlobServiceClient(
+        account_url=STORAGEACCOUNTURL, credential=STORAGEACCOUNTKEY)
+    blob_client_instance = blob_service_client_instance.get_blob_client(
+        CONTAINERNAME, BLOBNAME, snapshot=None)
+    blob_data = blob_client_instance.download_blob()
+    filebuffer = BytesIO()
+    blob_data.download_to_stream(filebuffer, max_concurrency=10)
+    filebuffer.seek(0)
+    return filebuffer
+
+# To upload blob into Bas Analyse
+
+
+def upload_df(data, container, blobpathfilename, STORAGEACCOUNTURL, STORAGEACCOUNTKEY,dictionary):
+    blob_service_client_instance = BlobServiceClient(
+        account_url=STORAGEACCOUNTURL, credential=STORAGEACCOUNTKEY)
+    logger.info('Uploading data')
+    blob_client = blob_service_client_instance.get_blob_client(
+        container=container, blob="{}.parquet".format(blobpathfilename))
+    parquet_file = BytesIO()
+    data.to_parquet(parquet_file, engine='pyarrow', use_dictionary=dictionary)
+    # change the stream position back to the beginning after writing
+    parquet_file.seek(0)
+    return blob_client.upload_blob(
+        data=parquet_file, overwrite=True
+    )
+
+# To upload pickle
+
+
+def upload_pickle(data, STORAGEACCOUNTURL, STORAGEACCOUNTKEY, CONTAINERNAME, BLOBNAME):
+    blob_service_client_instance = BlobServiceClient(
+        account_url=STORAGEACCOUNTURL, credential=STORAGEACCOUNTKEY)
+    logger.info('Uploading pickle')
+    blob_client = blob_service_client_instance.get_blob_client(
+        container=CONTAINERNAME, blob="/output/{}.pickle".format(BLOBNAME))
+    pickle_file = BytesIO()
+    pickle.dump(data, pickle_file)
+    # change the stream position back to the beginning after writing
+    pickle_file.seek(0)
+    return blob_client.upload_blob(
+        data=pickle_file, overwrite=True
+    )
+
+def download_pickle(STORAGEACCOUNTURL, STORAGEACCOUNTKEY,
+                    CONTAINERNAME, BLOBNAME, large_dataset):
+    logger.info(f'Downloading pickle')
+    blob_service_client_instance = BlobServiceClient(
+        account_url=STORAGEACCOUNTURL, credential=STORAGEACCOUNTKEY)
+    blob_client_instance = blob_service_client_instance.get_blob_client(
+        CONTAINERNAME, BLOBNAME, snapshot=None)
+    blob_data = blob_client_instance.download_blob()
+    filebuffer = BytesIO()
+    blob_data.download_to_stream(filebuffer, max_concurrency=10)
+    filebuffer.seek(0)
+    ob = pickle.load(filebuffer)
+    return ob
+
+
+def upload_from_filebuffer(STORAGEACCOUNTURL, STORAGEACCOUNTKEY,
+                           container, blob_path, filebuffer):
+    blob_service_client_instance = BlobServiceClient(
+        account_url=STORAGEACCOUNTURL, credential=STORAGEACCOUNTKEY)
+    blob_client = blob_service_client_instance.get_blob_client(
+        container=container, blob=blob_path)
+    filebuffer.seek(0)
+    blob_client.upload_blob(filebuffer, max_concurrency=10,
+                            overwrite=True)
+
+def get_client(
+        container, filename,
+        STORAGEACCOUNTURL, STORAGEACCOUNTKEY):
+    logger.info(f'Connect to blob client')
+    blob_service_client_instance = BlobServiceClient(
+        account_url=STORAGEACCOUNTURL,
+        credential=STORAGEACCOUNTKEY)
+    logger.info(f'Create blob instance to open blob filename')
+    container_client = blob_service_client_instance.get_blob_client(
+        container, blob="{}".format(
+            filename), snapshot=None)
+    return container_client
