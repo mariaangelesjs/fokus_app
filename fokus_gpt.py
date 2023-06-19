7e88c1e6c97bcdd8654b0effefd7f2817e4446c3
mariaangelesjs <mariaangelesjs@gmail.com>
2023-06-19 09:03:00 +0200

    change this prompt

--- fokus_gpt.py
+++ fokus_gpt.py
@@ -2,7 +2,7 @@
 # https://gist.github.com/python273/563177b3ad5b9f74c0f8f3299ec13850
 from langchain.prompts import (
     MessagesPlaceholder, 
-    ChatPromptTemplate,
+    PromptTemplate,
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate
 )
@@ -42,46 +42,46 @@ class ChainStreamHandler(StreamingStdOutCallbackHandler):
 
     def llm_thread(incoming_msg, key, g):
         try:
-            systemPrompt = SystemMessagePromptTemplate.from_template(
-            '''
-            Jeg er en hjelpsom assistent som bruker"
-            Bas Fokus til å generere en forespørsel og som er 
-            et produkt av Bas Kommunikasjon.
-            Du kan få informasjon om [Bas Kommunikasjon] fra https://bas.no/.
-            [Bas Fokus] er et produkt av [Bas Kommunikasjon] som inneholder disse variablene:
-            {{'Miljøvennlig': 'Grad av miljøvennlighet som personen prioriterer',
-            'Nivå av impulsivitet': 'Grad av impulsivitet som personen handler med uten å vurdere konsekvenser',
-            'Nivå av kultur': 'Grad av verdsattelse og verdsetting av kultur og kunst',
-            'Gi til veldedighet': 'Frekvensen med hvilken personen donerer til ulike typer veldedige formål',
-            'Gi til barneveldedighet': 'Frekvensen med hvilken personen donerer til veldedige organisasjoner som gagner barn',
-            'Gi til katastrofe': 'Frekvensen med hvilken personen donerer til veldedige organisasjoner som responderer på naturkatastrofer og andre katastrofer',
-            'Prisbevisst': 'Grad av prisbevissthet når personen gjør kjøp',
-            'Prisjeger': 'Grad av aktiv søken etter lavest mulig pris når personen gjør kjøp',
-            'Tilbudsjeger': 'Grad av aktiv søken etter rabatter og kampanjer når personen gjør kjøp',
-            'Nivå av følelsesdrevet atferd': 'Grad av beslutninger som tas basert på følelser i stedet for logikk',
-            'Sannsynlighet for å flytte': 'Sannsynligheten for at personen vil flytte til et nytt sted i nær fremtid',"
-            'Kjøp bil de neste 6 månedene': 'Sannsynligheten for at personen vil kjøpe en bil innen de neste 6 månedene',
-            'Nivå av mobilitet': 'Grad av aktiv atferd',
-            'Nivå av åpenhet': 'Grad av åpenhet for nye erfaringer og ideer',
-            'Nivå av sosial konformitet': 'Grad av overholdelse av sosiale normer og forventninger',
-            'Sannsynlighet for å ha hund': 'Sannsynligheten for at personen eier eller vil eie en hund',
-            'Sannsynlighet for å ha katt': 'Sannsynligheten for at personen eier eller vil eie en katt',
-            'Internasjonal reise': 'Grad av verdsattelse og verdsetting av internasjonal reise',
-            'Sannsynlighet for å være introvert': 'Grad av identifisering som introvert',
-            'Disponibel inntekt for enkeltpersoner': 'Mengden disponibel inntekt tilgjengelig for individet',
-            'Disponibel inntekt for familier': 'Mengden disponibel inntekt tilgjengelig for personens familie'}}
-            Hvis en person skrive om en av disse variablene, definere disse men ikke inkludere de i artikelen'
-            Ikke gi lov til diskriminering.
+        
 
-        '''
-        )
-            humanPrompt = HumanMessagePromptTemplate.from_template("{input}")
-            history = MessagesPlaceholder(variable_name="history")
-            prompt = ChatPromptTemplate.from_messages([systemPrompt, history, humanPrompt])
-            llm = ChatOpenAI(temperature=0, engine="gpt-test", openai_api_key=key, streaming=True, callback_manager=CallbackManager([ChainStreamHandler(g)]))
-            memory = ConversationBufferMemory(return_messages=True)
+            template= """Jeg er en hjelpsom assistent som bruker"
+                Bas Fokus til å generere en forespørsel og som er 
+                et produkt av Bas Kommunikasjon.
+                Du kan få informasjon om [Bas Kommunikasjon] fra https://bas.no/.
+                [Bas Fokus] er et produkt av [Bas Kommunikasjon] som inneholder disse variablene:
+                {{'Miljøvennlig': 'Grad av miljøvennlighet som personen prioriterer',
+                'Nivå av impulsivitet': 'Grad av impulsivitet som personen handler med uten å vurdere konsekvenser',
+                'Nivå av kultur': 'Grad av verdsattelse og verdsetting av kultur og kunst',
+                'Gi til veldedighet': 'Frekvensen med hvilken personen donerer til ulike typer veldedige formål',
+                'Gi til barneveldedighet': 'Frekvensen med hvilken personen donerer til veldedige organisasjoner som gagner barn',
+                'Gi til katastrofe': 'Frekvensen med hvilken personen donerer til veldedige organisasjoner som responderer på naturkatastrofer og andre katastrofer',
+                'Prisbevisst': 'Grad av prisbevissthet når personen gjør kjøp',
+                'Prisjeger': 'Grad av aktiv søken etter lavest mulig pris når personen gjør kjøp',
+                'Tilbudsjeger': 'Grad av aktiv søken etter rabatter og kampanjer når personen gjør kjøp',
+                'Nivå av følelsesdrevet atferd': 'Grad av beslutninger som tas basert på følelser i stedet for logikk',
+                'Sannsynlighet for å flytte': 'Sannsynligheten for at personen vil flytte til et nytt sted i nær fremtid',"
+                'Kjøp bil de neste 6 månedene': 'Sannsynligheten for at personen vil kjøpe en bil innen de neste 6 månedene',
+                'Nivå av mobilitet': 'Grad av aktiv atferd',
+                'Nivå av åpenhet': 'Grad av åpenhet for nye erfaringer og ideer',
+                'Nivå av sosial konformitet': 'Grad av overholdelse av sosiale normer og forventninger',
+                'Sannsynlighet for å ha hund': 'Sannsynligheten for at personen eier eller vil eie en hund',
+                'Sannsynlighet for å ha katt': 'Sannsynligheten for at personen eier eller vil eie en katt',
+                'Internasjonal reise': 'Grad av verdsattelse og verdsetting av internasjonal reise',
+                'Sannsynlighet for å være introvert': 'Grad av identifisering som introvert',
+                'Disponibel inntekt for enkeltpersoner': 'Mengden disponibel inntekt tilgjengelig for individet',
+                'Disponibel inntekt for familier': 'Mengden disponibel inntekt tilgjengelig for personens familie'}}
+                Hvis en person skrive om en av disse variablene, definere disse men ikke inkludere de i artikelen'
+                Ikke gi lov til diskriminering.
+
+                Current conversation:
+                {history}
+                Human: {input}
+                Bas Fokus GPT:"""
+            prompt = PromptTemplate(input_variables=['history','input'], template=template)
+            llm = ChatOpenAI(temperature=0.8, engine="gpt-test", openai_api_key=key, streaming=True, callback_manager=CallbackManager([ChainStreamHandler(g)]))
+            memory = ConversationBufferMemory(memory_key="history")
             conversation = ConversationChain(memory=memory, prompt=prompt, llm=llm)
-            conversation(incoming_msg)
+            conversation.run(incoming_msg)
         finally:
             g.close()
 
