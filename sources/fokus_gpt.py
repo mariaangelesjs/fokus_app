d937a8f86f4523a2dd57c6ac3d5b12b23199011d
mariaangelesjs <mariaangelesjs@gmail.com>
2023-06-22 10:21:21 +0200

    fix link

--- sources/fokus_gpt.py
+++ sources/fokus_gpt.py
@@ -110,10 +110,7 @@ class ChainStreamHandler(StreamingStdOutCallbackHandler):
                     * Rediger svar før du sender den ut.
                     * Vær forsiktig med tekstformat og fikse om du finner feil før du skriver svar.
                     * Skrive alle lenker i HTML format og Human kan trykke på lenke.
-                    * Alle lenker skal skrives som:
-                    "
-                    <a href="lenke" target="_blank">description</a>
-                    "
+                    * Alle lenker skal skrives som: <a href="lenke" target="_blank">description</a>
                     * Sjekk at de lenkene er bra skrevet.
                     * Ikke gi informasjon om chathistorikk når Human spørre "hvem er du?"
                     * Ha en snill "tone of voice".
