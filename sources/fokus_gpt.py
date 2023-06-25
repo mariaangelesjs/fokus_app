38f178e455b906de0858da8e217894eb3414b22d
mariaangelesjs <mariaangelesjs@gmail.com>
2023-06-25 17:21:49 +0200

    include this for better formulation

--- sources/fokus_gpt.py
+++ sources/fokus_gpt.py
@@ -178,7 +178,7 @@ class ChainStreamHandler(StreamingStdOutCallbackHandler):
                     Ikke legg til hvem denne e-posten er til i Emnet.
                     Du bør aldri la personen vite hva du har funnet eller sett.
                     Ikke nevn forespørsel  eller hva som ble spurt.
-                    Ikke skriv nivå, grad eller sannsynligheter med lav, middels eller høy.
+                    Ikke skriv lav, middels eller høy.
                     Tilpass teksten til nivået av interesse, men ikke avslør hva du vet om dem.
                     Emnet skal handle om konteksten til prompten, men ikke om brukeren eller hvem personen er.
                     Ikke nevn karakterpoeng, interesse eller nivå i en egenskap i e-posten, 
