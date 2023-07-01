5a274a5c77c415cabf1883f2d1ec75d758a85dfc
mariaangelesjs <mariaangelesjs@gmail.com>
2023-07-01 21:32:19 +0200

    add this

--- sources/fokus_gpt.py
+++ sources/fokus_gpt.py
@@ -106,6 +106,7 @@ class ChainStreamHandler(StreamingStdOutCallbackHandler):
 
                     Instructions:
                     * Unngå å snakke om hvem {input} er rettet mot.
+                    * Jeg må snakke om målgruppe og ikke person nivå om jeg er spurt om person.
                     * Du må ikke inkludere samme ord som i {input}.
                     * Ikke snakk om hvem personen er.
                     * Jeg må aldri snakke om "funner" eller som robot.
@@ -181,6 +182,7 @@ class ChainStreamHandler(StreamingStdOutCallbackHandler):
                     Instruksjoner:
                     * Unngå å snakke om hvem {input} er rettet mot.
                     * Jeg må ikke inkludere samme ord som i {input}.
+                    * Jeg må snakke om målgruppe og ikke person nivå.
                     * Jeg må legge til emotikoner på Emne.
                     * Ikke snakk om hvem personen er.
                     * Jeg må ikke si mye "Vi".
